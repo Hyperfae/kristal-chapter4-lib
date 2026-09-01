519cbbaca864ac8dfe5282a60076b6c7f359e139
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-09-01 11:05:18 -0700

    Update PlayerClimbState.lua

--- scripts/hooks/PlayerClimbState.lua
+++ scripts/hooks/PlayerClimbState.lua
@@ -183,7 +183,7 @@ function PlayerClimbState:drawReticleHint()
         Draw.setColor(col)
 		love.graphics.push()
 		love.graphics.origin()
-		local cx, cy, _, _ = Game.world.camera:getRect
+		local cx, cy, _, _ = Game.world.camera:getRect()
 		love.graphics.translate(-cx, -cy)
 		for subsection = 0, count - 1 do
 			local tilex = _tilex + ((subsection + 1) * shiftx)
