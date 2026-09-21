2c32f775fb26af9f409b8059b8afc275618ec3f6
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-09-21 07:54:52 -0700

    Compat climbcoin and remove assets already in Kristal

---
+++ scripts/objects/compat/ClimbCoin.lua
@@ -0,0 +1,12 @@
+---@class ClimbCoin : CoinBowl
+local ClimbCoin, super = Class(CoinBowl)
+
+---@param x number?
+---@param y number?
+---@param settings CoinBowlSettings?
+function ClimbCoin:init(x, y, settings)
+    super.init(self, x, y, settings)
+    Kristal.Console:warn("Replace ClimbCoin with CoinBowl!")
+end
+
+return ClimbCoin
