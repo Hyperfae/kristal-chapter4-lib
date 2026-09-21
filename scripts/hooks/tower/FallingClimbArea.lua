12fd67ea0047b54f3163642ced7bd7b601d41026
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-09-21 09:06:50 -0700

    Update FallingClimbArea.lua

--- scripts/hooks/tower/FallingClimbArea.lua
+++ scripts/hooks/tower/FallingClimbArea.lua
@@ -15,6 +15,9 @@ function FallingClimbArea:applyTileObject(data, map)
 
     tile:setPosition(ox * self.width, oy * self.height)
 
+    if Game.world.map.cyltower then
+		tile.visible = false
+	end
 	self.tile = tile
     self:addChild(tile)
 end
